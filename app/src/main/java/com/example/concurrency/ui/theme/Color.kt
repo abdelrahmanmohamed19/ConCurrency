d9062d1ed65e5d1f5b7a0858f024cb91b968b23c
mahmoud adel <mahmoudadel5556@gmail.com>
2023-08-23 22:44:29 +0300

    final design for convert & compare

--- app/src/main/java/com/example/concurrency/ui/theme/Color.kt
+++ app/src/main/java/com/example/concurrency/ui/theme/Color.kt
@@ -20,24 +20,3 @@ val CardComponentBackground = Color(0xFFE7E6E6)
 val CardBorderColor = Color(0xFFC5C5C5)
 val CardTextColor = Color(0xFF000000)
 val CardButtonBackground = Color(0xFF363636)
-
-// Tab Colors
-val UnSelectedTabColor = Color(0xFFF8F8F8)
-val SelectedTabColor = Color(0xFFFFFFFF)
-val TabTextColor     = Color(0xFF141414)
-
-
-
-
-
-
-
-
-
-fun mixColors(color1: Color, color2: Color, weight: Float): Color {
-    val r = (color1.red * weight) + (color2.red * (1 - weight))
-    val g = (color1.green * weight) + (color2.green * (1 - weight))
-    val b = (color1.blue * weight) + (color2.blue * (1 - weight))
-    val a = (color1.alpha * weight) + (color2.alpha * (1 - weight))
-    return Color(r, g, b, a)
-}
